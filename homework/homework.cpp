5cc9fc0419c2763582ebe7d61d8dd67b4e5d2255
snzhrk <sanzhar.kazakstan@gmail.com>
2025-10-08 11:06:37 +0200

    homework-1 first draft of the hw

--- homework/homework.cpp
+++ homework/homework.cpp
@@ -1,5 +1,5 @@
-import <string>;
-import <iostream>;
+#include <string>
+#include <iostream>
 
 using namespace std;
 
@@ -17,15 +17,62 @@ public:
     Coffee(const string &name) : Coffee(name, 25){}
     Coffee() : Coffee("empty", 0){}
 
-    string getName() const {
-        return this->name;}
+    const string& getName() const {
+        return this->name;
+    }
+
+    string fullInfo() const{
+        return "Coffee<" + this->name + " , " + to_string(weight) + ">";
+    }
 
     unsigned getWeight() const {
           return this->weight;}
 
-    bool drink(unsigned amount) {
+    bool drink(unsigned int amount) {
         if (this->getWeight() < amount) {return false;}
         this->weight -= amount;
         return true;
+    }
+};
+
+class CoffeeFactory {
+private:
+    string brand;
+    unsigned weight;
+    unsigned serial;
+
+public:
+    CoffeeFactory(const string &brand, unsigned weight) {
+        this->brand = brand;
+        this->weight = weight;
+        this->serial = 0;
+    }
+    CoffeeFactory(const string &brand) : CoffeeFactory(brand, 25) {}
+
+    Coffee package() {
+        Coffee newCoffeePkg = Coffee(this->brand + " " + to_string(this->serial), this->weight);
+        this->serial++;
+        return newCoffeePkg;
+    }
+};
+
+class CoffeeStorage {
+private:
+    Coffee storage[3];
+
+public:
+    CoffeeStorage() {
+        for (auto & i : storage) {
+            i = Coffee();
+        }
+    }
+
+    Coffee& getByIdx(const unsigned index) {
+        return storage[index];
+    }
+
+    bool drink(unsigned index, unsigned amount) {
+        if (index >= 3) {return false;}
+        return getByIdx(index).drink(amount);
     }
 };
